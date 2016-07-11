518d57a50b2e4f6b0ecbc20c5987f0b98715b1cb
jmsunico <jmsunico@gmail.com>
2016-07-11 11:26:32 +0200

    Main logic working, now pendint to implement requested user interface/behaviour

--- openLibrary3/openLibrary3/myCollectionViewController.swift
+++ openLibrary3/openLibrary3/myCollectionViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 private let reuseIdentifier = "Cell"
 
 class myCollectionViewController: UICollectionViewController {
-
+/*
 	struct Section{
 		var name: String
 		var images: [UIImage]
@@ -21,12 +21,53 @@ class myCollectionViewController: UICollectionViewController {
 		}
 	}
 	var sections = [Section]()
+*/
+	struct Section{
+		var isbn: String
+		var title: String
+		var authors: String
+		var pubdate: String
+		var url: String
+		var keywords: String
+		var errors: String
+		var cover: UIImage
+		init(isbn: String){
+			self.isbn = isbn
+			self.title = ""
+			self.authors = ""
+			self.pubdate = ""
+			self.url = ""
+			self.keywords = ""
+			self.errors = ""
+			self.cover = UIImage(named: "pending")!
+		}
+	}
+	var sections = [Section]()
 	
 	
 	@IBAction func searchTextField(sender: UITextField) {
-		let section = Section(name: sender.text!, images: googleCustomSearch(sender.text!))
-		sections.append(section)
-		self.collectionView!.reloadData()
+		let newISBN : String? = sender.text!.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+		if (newISBN != nil) && (newISBN! != ""){
+			let newSection = searchISBN(newISBN!)
+			if (newSection.errors == ""){
+				sections.append(newSection)
+				self.collectionView!.reloadData()
+			}
+			else{
+				//ANALYSE ERRORS!!!
+				let alert = UIAlertController(title: "ERRORS!", message: "newSection.errors", preferredStyle: .Alert)
+				let accion = UIAlertAction(title: "OK", style: .Default, handler: nil)
+				alert.addAction(accion)
+				self.presentViewController(alert, animated: true, completion: nil)
+			}
+		}
+	}
+/*
+	@IBAction func ISBN1(sender: AnyObject) {
+	self.isbnText.text = "978-84-376-0494-7"
+	}
+	@IBAction func ISBN2(sender: AnyObject) {
+	self.isbnText.text = "978-84-973-6467-6"
 	}
 	
 	func googleCustomSearch (term: String) -> [UIImage]{
@@ -51,6 +92,122 @@ class myCollectionViewController: UICollectionViewController {
 			print("Something nasty happened...")
 		}
 		return images
+	}
+*/
+	
+	func searchISBN(realISBN: String) -> Section{
+		var newSection = Section(isbn: realISBN)
+		
+		let query = String("https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + newSection.isbn)
+		let myQuery = NSURL(string: query)
+		if myQuery == nil{
+			newSection.errors = "NO VALID QUERY!"
+			return newSection
+		}
+		
+		let jsonAnswer: AnyObject?
+		do{ //trying to process response
+			let theirResponse = NSData(contentsOfURL: myQuery!)
+			if theirResponse == nil {
+				throw NSURLError.BadServerResponse
+			}
+			jsonAnswer = try NSJSONSerialization.JSONObjectWithData((theirResponse)!, options: NSJSONReadingOptions.MutableLeaves)
+		}
+		catch _{
+			newSection.errors = "NO INTERNET CONNECTION!"
+			return newSection
+		}
+		
+		let dictAnswer = jsonAnswer as? NSDictionary
+		if dictAnswer == nil {
+			newSection.errors = "NO VALID ANSWER RECEIVED!"
+			return newSection
+		}
+		
+		let dictISBN = dictAnswer!["ISBN:" + newSection.isbn] as? NSDictionary
+		if dictISBN == nil {
+			newSection.errors = "NO VALID ISBN FIELD!"
+			return newSection
+		}
+		
+		let dictISBN_title = dictISBN!["title"] as? String
+		if dictISBN_title == nil {
+			newSection.errors = "NO TITLE FOUND!"
+			return newSection
+		} else{
+			newSection.title = dictISBN_title!
+		}
+		
+		let dictISBN_cover = dictISBN!["cover"] as? NSDictionary
+		if dictISBN_cover != nil{
+			let smallCoverString = dictISBN_cover!["small"] as? String
+			let mediumCoverString = dictISBN_cover!["medium"] as? String
+			let largeCoverString = dictISBN_cover!["large"] as? String
+			if largeCoverString != nil {
+				let coverURL = NSURL(string: largeCoverString!)
+				let coverDATA = NSData(contentsOfURL: coverURL!)
+				if coverDATA != nil{
+					if let coverImage = UIImage(data: coverDATA!){
+						newSection.cover = coverImage
+					}
+				}
+			}
+			else if mediumCoverString != nil {
+				let coverURL = NSURL(string: mediumCoverString!)
+				let coverDATA = NSData(contentsOfURL: coverURL!)
+				if coverDATA != nil{
+					if let coverImage = UIImage(data: coverDATA!){
+						newSection.cover = coverImage
+					}
+				}
+			}
+			else if smallCoverString != nil {
+				let coverURL = NSURL(string: smallCoverString!)
+				let coverDATA = NSData(contentsOfURL: coverURL!)
+				if coverDATA != nil{
+					if let coverImage = UIImage(data: coverDATA!){
+						newSection.cover = coverImage
+					}
+				}
+			}
+			else {
+				newSection.errors = "WE SHOULD NEVER BE HERE!"
+				return newSection
+			}
+		}
+		
+		let dictISBN_yearpub = dictISBN!["publish_date"] as? String
+		if dictISBN_yearpub != nil {
+			newSection.pubdate = dictISBN_yearpub!
+		}
+			
+		let dictISBN_url = dictISBN!["url"] as? String
+		if dictISBN_url != nil {
+			newSection.url = dictISBN_url!
+		}
+			
+		let arrayAuthors = dictISBN!["authors"] as? NSArray
+		if arrayAuthors != nil {
+			var authors = ""
+			for index in 0..<arrayAuthors!.count{
+				let tempDict = arrayAuthors![index] as! NSDictionary
+				authors = authors + (tempDict["name"] as! String) + " ("
+				authors = authors + (tempDict["url"] as! String) + ")\n"
+			}
+			newSection.authors = authors // may be change into an array if needed...
+		}
+		
+		let arraySubjects = dictISBN!["subjects"] as? NSArray
+		if arraySubjects != nil{
+			var keywords = ""
+			for index in 0..<arraySubjects!.count{
+				let tempDict = arraySubjects![index] as! NSDictionary
+				keywords = keywords + (tempDict["name"] as! String) + " "
+			}
+			newSection.keywords = keywords
+		}
+		print(String(jsonAnswer!)) // keep this for testing...
+		return newSection
 	}
 	
     override func viewDidLoad() {
@@ -90,12 +247,13 @@ class myCollectionViewController: UICollectionViewController {
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return self.sections[section].images.count
+        //return self.sections[section].images.count
+		return 1
     }
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! cellView
-		cell.cellImage.image = sections[indexPath.section].images[indexPath.item]
+		cell.cellImage.image = sections[indexPath.section].cover
 		
         // Configure the cell
     
@@ -136,7 +294,7 @@ class myCollectionViewController: UICollectionViewController {
 	override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
 		let cell2 = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "Cell2", forIndexPath: indexPath) as! sectionHeader
 		
-		cell2.sectionHeaderLabel.text = sections[indexPath.section].name
+		cell2.sectionHeaderLabel.text = sections[indexPath.section].title
 		return cell2
 	}
 	
